883c23316eca998ce7c613ae4a2f846a144d23b1
nikethan reddy <nikethan.nicky@gmail.com>
2020-12-14 20:36:07 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+var contentList = [];
+var authorList = [];
+var composeDate = [];
+contentList.push(homeStartingContent);
+authorList.push("Admin");
+
+var currentDate = function(){
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+}
+composeDate.push(currentDate());
+
+
+
+app.get("/",function(req,res){
+    res.render("home",{authorList : authorList , contentList: contentList , composeDate : composeDate});
+    
+});
+app.get("/about",function(req,res){
+    res.render("about",{about:aboutContent});
+});
+
+app.get("/contact",function(req,res){
+    res.render("contact",{contact: contactContent})
+});
+
+app.get("/compose",function(req,res){
+    res.render("compose");
+    
+    
+})
+
+app.post("/compose",function(req,res){
+    var text = req.body.authorName;
+    var content = req.body.content;
+    console.log(text+" : "+content);
+    contentList.push(content);
+    authorList.push(text);
+    composeDate.push(currentDate());
+    
+    res.redirect("/");
+    
+    
+});
+app.post("/",function(req,res){
+    res.redirect("/compose");
+})
+
+
+app.get("/posts/:testing",function(req,res){
+    res.send(req.params.testing);
+})
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
